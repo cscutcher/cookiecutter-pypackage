d380cfa3b63e3958e9a8c596869cd7df0df5129e
Chris Scutcher <chris@scutcher.co.uk>
2017-03-23 17:19:38 +0000

    Change some defaults to my preferences

--- tests/test_bake_project.py
+++ tests/test_bake_project.py
@@ -187,7 +187,7 @@ def test_using_pytest(cookies):
 
 
 def test_not_using_pytest(cookies):
-    with bake_in_temp_dir(cookies) as result:
+    with bake_in_temp_dir(cookies, extra_context={'use_pytest': 'n'}) as result:
         assert result.project.isdir()
         test_file_path = result.project.join('tests/test_python_boilerplate.py')
         lines = test_file_path.readlines()
